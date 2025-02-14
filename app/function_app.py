0f69647dc0822bb6f4ec756edd0f56cb7ef19b60
Ankit Desai <ankitdesai@microsoft.com>
2025-02-14 10:17:43 -0500

    make anonymous

--- app/function_app.py
+++ app/function_app.py
@@ -72,7 +72,7 @@ def initialize_client():
 
     return project_client, thread, agent
 
-@app.route(route="prompt", auth_level=func.AuthLevel.FUNCTION)
+@app.route(route="prompt", auth_level=func.AuthLevel.ANONYMOUS)
 def prompt(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
