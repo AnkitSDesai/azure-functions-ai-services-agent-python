43bf64ba42f97b871ea8e89f08672a73269d1873
Ankit Desai <ankitdesai@microsoft.com>
2025-02-14 16:01:10 -0500

    revert open tela

--- app/function_app.py
+++ app/function_app.py
@@ -6,48 +6,19 @@ import time
 from azure.ai.projects import AIProjectClient
 from azure.identity import DefaultAzureCredential
 from azure.storage.queue import QueueClient, BinaryBase64EncodePolicy, BinaryBase64DecodePolicy
-from azure.core.pipeline.policies import DistributedTracingPolicy
-from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
-from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 app = func.FunctionApp()
-
 
 # Name of the queues to get and send the function call messages
 input_queue_name = "input"
 output_queue_name = "output"
-
-# Initialize tracing
-def initialize_tracing():
-    resource = Resource(attributes={
-        "service.name": "azure-functions-ai-services-agent-python"
-    })
-    provider = TracerProvider(resource=resource)
-    trace.set_tracer_provider(provider)
-
-    # Configure the OTLP exporter
-    otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4317", insecure=True)
-    provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
-
-    # Configure the Azure Monitor exporter
-    azure_monitor_exporter = AzureMonitorTraceExporter.from_connection_string(
-        os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
-    )
-    provider.add_span_processor(BatchSpanProcessor(azure_monitor_exporter))
-
-initialize_tracing()
 
 # Function to initialize the agent client and the tools Azure Functions that the agent can use
 def initialize_client():
     # Create a project client using the connection string from local.settings.json
     project_client = AIProjectClient.from_connection_string(
         credential=DefaultAzureCredential(),
-        conn_str=os.environ["PROJECT_CONNECTION_STRING"],
-        policies=[DistributedTracingPolicy()]
+        conn_str=os.environ["PROJECT_CONNECTION_STRING"]
     )
 
     # Get the connection string from local.settings.json
